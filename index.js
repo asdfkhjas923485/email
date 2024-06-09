34e47725e8f16b6fe114e5745fde291c19ab6d91
maarifalawi <115444345+maarifalawi@users.noreply.github.com>
2024-06-10 02:21:55 +0700

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function sendMail() {
   var params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+
     message: document.getElementById("message").value,
   };
 
